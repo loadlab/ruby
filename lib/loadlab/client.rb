f3d40cbb776746b2e80b4b56d1007b3ca5988fdc
Michael Martinez <michaelmartinez@me.com>
2018-09-16 16:10:10 -0600

    Adds base API for LoadLab ruby

--- lib/loadlab/client.rb
+++ lib/loadlab/client.rb
@@ -1,39 +1,31 @@
+require 'httparty'
+
 class Resource
-    include HTTParty
-    PATH = nil
     BASE_URL = 'https://api.loadlab.co/v1'
 
-    @api_token = api_token
-    @headers = {
-        Authorization: "Token #{api_token}",
-        Accept: 'application/json',
-    }    
-
-    def get
-        self.class.get(self.BASE_URL + self.PATH, @headers)
+    def initialize(api_token, path)
+        @path = path
+        @headers = {
+            Authorization: "Token #{api_token}",
+            Accept: 'application/json',
+        }         
     end
 
-end
+    def get
+        HTTParty.get(BASE_URL + @path, headers: @headers)
+    end
 
-class Plans < Resource
-    PATH = '/plans/'
-end
-
-class Jobs < Resource
-    PATH = '/jobs/'
-end
-
-class Sites < Resource
-    PATH = '/sites/'
 end
 
 
 module LoadLab
     class Client
+        attr_reader :plans, :jobs, :sites
+
         def initialize api_token
-            self.plans = Plans.new(api_token)
-            self.pages = Jobs.new(api_token)
-            self.sites = Sites.new(api_token)
+            @plans = Resource.new(api_token, '/plans/')
+            @jobs = Resource.new(api_token, '/jobs/')
+            @sites = Resource.new(api_token, '/sites/')
         end  
     end
 end
--- spec/loadlab_spec.rb
+++ spec/loadlab_spec.rb
@@ -1,13 +1,25 @@
+require 'spec_helper'
+
 RSpec.describe LoadLab do
+
+  client = LoadLab::Client.new(ENV['LOADLAB_API_TOKEN'])
   
   it "has a version number" do
     expect(LoadLab::VERSION).not_to be nil
   end
 
-  it "I can instantiate it" do
-    client = LoadLab::Client.new('xxxx')
-    sites = client.sites.get
-    puts sites
-    expect(true).to eq(true)
+  it "I can get jobs" do
+    response = client.jobs.get
+    expect(response.keys).to eq(["count", "next", "previous", "results"])
+  end
+
+  it "I can get plans" do
+    response = client.plans.get
+    expect(response.keys).to eq(["count", "next", "previous", "results"])
+  end
+
+  it "I can get sites" do
+    response = client.sites.get
+    expect(response.keys).to eq(["count", "next", "previous", "results"])
   end
 end
