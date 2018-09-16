1286f6deef1e9cbbfeff1f8cc0b6c7b004ecb6e4
Michael Martinez <michaelmartinez@me.com>
2018-09-16 14:45:08 -0600

    Checkpoint commit

--- lib/loadlab/loadlab.rb
+++ lib/loadlab/loadlab.rb
@@ -1,7 +1,2 @@
 require "loadlab/version"
 require "loadlab/client"
-
-
-module LoadLab
-    
-end
--- spec/loadlab_spec.rb
+++ spec/loadlab_spec.rb
@@ -1,10 +1,10 @@
 RSpec.describe LoadLab do
+  
   it "has a version number" do
     expect(LoadLab::VERSION).not_to be nil
   end
 
   it "I can instantiate it" do
-    puts LoadLab.constants
     client = LoadLab::Client.new('xxxx')
     sites = client.sites.get
     puts sites
